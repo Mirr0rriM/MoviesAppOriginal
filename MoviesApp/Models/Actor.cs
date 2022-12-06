f2015a25db813dcf7e638cd41b4a156236fb434f
Redrick Clicks <den-draks@yandex.ru>
2022-12-06 17:19:12 +0300

    commit

---
+++ MoviesApp/Models/Actor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MoviesApp.Models;
+
+public class Actor
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Surname { get; set; }
+    
+    [DataType(DataType.Date)]
+    public DateTime BirthDate { get; set; }
+}
